149b95652fa23e0938c8bcaadc3a243a060ca0d6
Остапчук Дмитрий <ostapchuk.d4@wb.ru>
2025-06-11 01:20:02 +0300

    fix tests

--- tests/ui/test_login_page.py
+++ tests/ui/test_login_page.py
@@ -1,21 +1,27 @@
+import allure
 from playwright.sync_api import expect
 
 from constants.constants import UI_MOVIES_URL, LOGIN_ENDPOINT
 
 
-def test_login(page, common_user):
-    page.goto(UI_MOVIES_URL + LOGIN_ENDPOINT)
+@allure.epic("Тестирование работы UI")
+@allure.feature("Тестирование взаимодействия авторизованного пользователя с UI")
+class TestLoginUser:
+    @allure.title("Тест на авторизацию существующего пользователя")
+    def test_login(self, page, common_user):
 
-    email_locator = "[data-qa-id='login_email_input']"
-    password_locator = "[data-qa-id='login_password_input']"
+        page.goto(UI_MOVIES_URL + LOGIN_ENDPOINT)
 
-    # page.pause()
-    page.fill(selector=email_locator, value=common_user.email)
-    page.fill(selector=password_locator, value=common_user.password)
+        email_locator = "[data-qa-id='login_email_input']"
+        password_locator = "[data-qa-id='login_password_input']"
 
-    login_locator = "[data-qa-id='login_submit_button']"
+        # page.pause()
+        page.fill(selector=email_locator, value=common_user.email)
+        page.fill(selector=password_locator, value=common_user.password)
 
-    page.click(login_locator)
+        login_locator = "[data-qa-id='login_submit_button']"
 
-    page.wait_for_url(UI_MOVIES_URL)
-    expect(page.get_by_text("Вы вошли в аккаунт")).to_be_visible(visible=True)
+        page.click(login_locator)
+
+        page.wait_for_url(UI_MOVIES_URL)
+        expect(page.get_by_text("Вы вошли в аккаунт")).to_be_visible(visible=True)
